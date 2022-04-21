f631179fdd35696aa195daee899c5c870714487d
wleite <wleite@student.42sp.org.br>
2022-04-20 23:20:35 -0300

    test: added stack

---
+++ test/stack/stack_test_1.cpp
@@ -0,0 +1,235 @@
+// Copyright (c) 2022 Welton Leite, wleite. All rights reserved.
+
+#include <iostream>
+
+#define KO "\033[1;31mKO\033[0m"
+#define OK "\033[1;32mOK\033[0m"
+
+#include <stack>
+
+#include "../../stack.hpp"
+
+bool pred(int i, int j) {
+  return (i == j);
+}
+
+int main(void) {
+  std::cout << "\n===========[ CONSTRUCTORS ]===========\n";
+  {
+    std::cout << "\n[ CONSTRUCTOR 1 ]\n";
+    ft::stack<int> FTsta;
+    std::stack<int> ORsta;
+    for (size_t i = 0; i < 10; i++) {
+      FTsta.push(i);
+      ORsta.push(i);
+    }
+    if (FTsta.top() == ORsta.top())
+      std::cout << OK " ";
+    else
+      std::cout << KO " ";
+  }
+
+  std::cout << "\n\n===========[ MEMBER FUNCTIONS ]===========\n";
+
+  {
+    std::cout << "\n[ EMPTY ]\n";
+    ft::stack<int> FTsta;
+    std::stack<int> ORsta;
+    if (FTsta.empty() == ORsta.empty())
+      std::cout << OK " ";
+    else
+      std::cout << KO " ";
+  }
+  {
+    ft::stack<int> FTsta;
+    std::stack<int> ORsta;
+    for (size_t i = 0; i < 10; i++) {
+      FTsta.push(i);
+      ORsta.push(i);
+    }
+    for (size_t i = 0; i < 10; i++) {
+      FTsta.pop();
+      ORsta.pop();
+    }
+    if (FTsta.size() == ORsta.size() && FTsta.empty() == ORsta.empty())
+      std::cout << OK " ";
+    else
+      std::cout << KO " ";
+  }
+
+  {
+    std::cout << "\n[ SIZE ]\n";
+    ft::stack<int> FTsta;
+    std::stack<int> ORsta;
+    if (FTsta.size() == ORsta.size())
+      std::cout << OK " ";
+    else
+      std::cout << KO " ";
+  }
+  {
+    ft::stack<int> FTsta;
+    std::stack<int> ORsta;
+    for (size_t i = 0; i < 10; i++) {
+      FTsta.push(i);
+      ORsta.push(i);
+    }
+    if (FTsta.size() == ORsta.size())
+      std::cout << OK " ";
+    else
+      std::cout << KO " ";
+  }
+
+  {
+    std::cout << "\n[ TOP ]\n";
+    ft::stack<int> FTsta;
+    std::stack<int> ORsta;
+    FTsta.push(42);
+    ORsta.push(42);
+    if (FTsta.top() == ORsta.top())
+      std::cout << OK " ";
+    else
+      std::cout << KO " ";
+  }
+  {
+    ft::stack<int> FTsta;
+    std::stack<int> ORsta;
+    for (size_t i = 0; i < 10; i++) {
+      FTsta.push(i);
+      ORsta.push(i);
+    }
+    if (FTsta.top() == ORsta.top())
+      std::cout << OK " ";
+    else
+      std::cout << KO " ";
+  }
+
+  {
+    std::cout << "\n[ PUSH ]\n";
+    ft::stack<int> FTsta;
+    std::stack<int> ORsta;
+    for (size_t i = 0; i < 10; i++) {
+      FTsta.push(i);
+      ORsta.push(i);
+    }
+    if (FTsta.top() == ORsta.top())
+      std::cout << OK " ";
+    else
+      std::cout << KO " ";
+  }
+
+  {
+    std::cout << "\n[ POP ]\n";
+    ft::stack<int> FTsta;
+    std::stack<int> ORsta;
+    for (size_t i = 0; i < 10; i++) {
+      FTsta.push(i);
+      ORsta.push(i);
+    }
+    for (size_t i = 0; i < 5; i++) {
+      FTsta.pop();
+      ORsta.pop();
+    }
+    if (FTsta.top() == ORsta.top())
+      std::cout << OK " ";
+    else
+      std::cout << KO " ";
+  }
+
+  std::cout << "\n\n===========[ RELATIONAL OPERATORS ]===========\n";
+
+  {
+    std::cout << "\n[ OPERATOR == ]\n";
+    ft::stack<int> FTsta1;
+    ft::stack<int> FTsta2;
+    for (int i = 0; i < 10; i++) {
+      FTsta1.push(i);
+      FTsta2.push(i);
+    }
+    if (FTsta1 == FTsta2)
+      std::cout << OK " ";
+    else
+      std::cout << KO " ";
+  }
+
+  {
+    std::cout << "\n[ OPERATOR != ]\n";
+    ft::stack<int> FTsta1;
+    ft::stack<int> FTsta2;
+    for (int i = 0; i < 10; i++) {
+      FTsta1.push(i);
+      FTsta2.push(i + 10);
+    }
+    if (FTsta1 != FTsta2)
+      std::cout << OK " ";
+    else
+      std::cout << KO " ";
+  }
+
+  {
+    std::cout << "\n[ OPERATOR < ]\n";
+    ft::stack<int> FTsta1;
+    ft::stack<int> FTsta2;
+    for (int i = 0; i < 9; i++) {
+      FTsta1.push(i);
+    }
+    for (int i = 0; i < 10; i++) {
+      FTsta2.push(i);
+    }
+    if (FTsta1 < FTsta2)
+      std::cout << OK " ";
+    else
+      std::cout << KO " ";
+  }
+
+  {
+    std::cout << "\n[ OPERATOR <= ]\n";
+    ft::stack<int> FTsta1;
+    ft::stack<int> FTsta2;
+    for (int i = 0; i < 10; i++) {
+      FTsta1.push(i);
+    }
+    for (int i = 0; i < 10; i++) {
+      FTsta2.push(i);
+    }
+    if (FTsta1 <= FTsta2)
+      std::cout << OK " ";
+    else
+      std::cout << KO " ";
+  }
+
+  {
+    std::cout << "\n[ OPERATOR > ]\n";
+    ft::stack<int> FTsta1;
+    ft::stack<int> FTsta2;
+    for (int i = 0; i < 10; i++) {
+      FTsta1.push(i);
+    }
+    for (int i = 0; i < 9; i++) {
+      FTsta2.push(i);
+    }
+    if (FTsta1 > FTsta2)
+      std::cout << OK " ";
+    else
+      std::cout << KO " ";
+  }
+
+  {
+    std::cout << "\n[ OPERATOR >= ]\n";
+    ft::stack<int> FTsta1;
+    ft::stack<int> FTsta2;
+    for (int i = 0; i < 10; i++) {
+      FTsta1.push(i);
+    }
+    for (int i = 0; i < 10; i++) {
+      FTsta2.push(i);
+    }
+    if (FTsta1 >= FTsta2)
+      std::cout << OK " ";
+    else
+      std::cout << KO " ";
+  }
+
+  std::cout << "\n";
+
+  return (0);
+}
---
+++ test/stack/stack_test_2.cpp
@@ -0,0 +1,179 @@
+// Copyright (c) 2022 Welton Leite, wleite. All rights reserved.
+
+#include <iostream>
+
+#if STD == 1
+#include <stack>
+namespace ft = std;
+#else
+#include "../../stack.hpp"
+#endif
+
+template <typename T>
+void print_stack(T& stack) {
+  for (size_t i = 0; i < stack.size(); i++) {
+    std::cout << stack.top() << " ";
+  }
+  std::cout << "\n";
+  std::cout << "size: " << stack.size() << "\n";
+}
+
+template <typename T>
+void fill_stack(T& stack, int size) {
+  for (int i = 0; i < size; i++) {
+    stack.push(i);
+  }
+}
+
+int main(void) {
+  std::cout << "\n===========[ CONSTRUCTORS ]===========\n";
+  {
+    std::cout << "\n[ CONSTRUCTOR 1 ]\n";
+    ft::stack<int> FTsta;
+    fill_stack(FTsta, 10);
+    print_stack(FTsta);
+  }
+
+  std::cout << "\n\n===========[ MEMBER FUNCTIONS ]===========\n";
+
+  {
+    std::cout << "\n[ EMPTY ]\n";
+    ft::stack<int> FTsta;
+    std::cout << "is empty? " << FTsta.empty() << "\n";
+  }
+  {
+    ft::stack<int> FTsta;
+    for (size_t i = 0; i < 10; i++) {
+      FTsta.push(i);
+    }
+    for (size_t i = 0; i < 10; i++) {
+      FTsta.pop();
+    }
+    std::cout << "is empty? " << FTsta.empty() << "\n";
+  }
+
+  {
+    std::cout << "\n[ SIZE ]\n";
+    ft::stack<int> FTsta;
+    fill_stack(FTsta, 5);
+    print_stack(FTsta);
+  }
+  {
+    ft::stack<int> FTsta;
+    fill_stack(FTsta, 10);
+    print_stack(FTsta);
+  }
+
+  {
+    std::cout << "\n[ TOP ]\n";
+    ft::stack<int> FTsta;
+    FTsta.push(42);
+    std::cout << "top? " << FTsta.top() << "\n";
+  }
+  {
+    ft::stack<int> FTsta;
+    fill_stack(FTsta, 10);
+    std::cout << "top? " << FTsta.top() << "\n";
+  }
+
+  {
+    std::cout << "\n[ PUSH ]\n";
+    ft::stack<int> FTsta;
+    for (size_t i = 0; i < 10; i++) {
+      FTsta.push(42);
+    }
+    print_stack(FTsta);
+  }
+
+  {
+    std::cout << "\n[ POP ]\n";
+    ft::stack<int> FTsta;
+    for (size_t i = 0; i < 10; i++) {
+      FTsta.push(i);
+    }
+    for (size_t i = 0; i < 5; i++) {
+      FTsta.pop();
+    }
+    print_stack(FTsta);
+  }
+
+  std::cout << "\n\n===========[ RELATIONAL OPERATORS ]===========\n";
+
+  {
+    std::cout << "\n[ OPERATOR == ]\n";
+    ft::stack<int> FTsta1;
+    ft::stack<int> FTsta2;
+    for (int i = 0; i < 10; i++) {
+      FTsta1.push(i);
+      FTsta2.push(i);
+    }
+    std::cout << "is equal? " << (FTsta1 == FTsta2) << "\n";
+  }
+
+  {
+    std::cout << "\n[ OPERATOR != ]\n";
+    ft::stack<int> FTsta1;
+    ft::stack<int> FTsta2;
+    for (int i = 0; i < 10; i++) {
+      FTsta1.push(i);
+      FTsta2.push(i + 10);
+    }
+    std::cout << "is different? " << (FTsta1 != FTsta2) << "\n";
+  }
+
+  {
+    std::cout << "\n[ OPERATOR < ]\n";
+    ft::stack<int> FTsta1;
+    ft::stack<int> FTsta2;
+    for (int i = 0; i < 9; i++) {
+      FTsta1.push(i);
+    }
+    for (int i = 0; i < 10; i++) {
+      FTsta2.push(i);
+    }
+    std::cout << "is minor? " << (FTsta1 < FTsta2) << "\n";
+  }
+
+  {
+    std::cout << "\n[ OPERATOR <= ]\n";
+    ft::stack<int> FTsta1;
+    ft::stack<int> FTsta2;
+    for (int i = 0; i < 10; i++) {
+      FTsta1.push(i);
+    }
+    for (int i = 0; i < 10; i++) {
+      FTsta2.push(i);
+    }
+    std::cout << "is minor or equal? " << (FTsta1 <= FTsta2) << "\n";
+  }
+
+  {
+    std::cout << "\n[ OPERATOR > ]\n";
+    ft::stack<int> FTsta1;
+    ft::stack<int> FTsta2;
+    for (int i = 0; i < 10; i++) {
+      FTsta1.push(i);
+    }
+    for (int i = 0; i < 9; i++) {
+      FTsta2.push(i);
+    }
+    std::cout << "is bigger? " << (FTsta1 > FTsta2) << "\n";
+  }
+
+  {
+    std::cout << "\n[ OPERATOR >= ]\n";
+    ft::stack<int> FTsta1;
+    ft::stack<int> FTsta2;
+    for (int i = 0; i < 10; i++) {
+      FTsta1.push(i);
+    }
+    for (int i = 0; i < 10; i++) {
+      FTsta2.push(i);
+    }
+    std::cout << "is bigger or equal? " << (FTsta1 >= FTsta2) << "\n";
+  }
+
+  std::cout << "\n";
+
+  return (0);
+}
