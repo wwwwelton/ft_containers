d829fa53b7c0811171f5f82c0ee309bf4847e02b
wleite <wleite@student.42sp.org.br>
2022-04-30 03:13:24 -0300

    refactor: changed red-black tree node to template

--- rb_tree_node.hpp
+++ rb_tree_node.hpp
@@ -10,26 +10,23 @@ enum Rb_tree_color {
   BLACK = false
 };
 
-struct Rb_tree_node {
-  typedef Rb_tree_node* Node_ptr;
-  typedef const Rb_tree_node* Const_node_ptr;
+template <typename T>
+struct _Rb_tree_node {
+  typedef _Rb_tree_node<T>* Node_ptr;
+  typedef const _Rb_tree_node<T>* Const_node_ptr;
 
-  int data;
+  T data;
   Node_ptr parent;
   Node_ptr left;
   Node_ptr right;
   Rb_tree_color color;
 
-  Rb_tree_node(int _data,
-               Node_ptr _parent,
-               Node_ptr _left,
-               Node_ptr _right,
-               Rb_tree_color _color)
+  explicit _Rb_tree_node(const T& _data)
       : data(_data),
-        parent(_parent),
-        left(_left),
-        right(_right),
-        color(_color) {}
+        parent(NULL),
+        left(NULL),
+        right(NULL),
+        color(BLACK) {}
 };
 
 }  // namespace ft
