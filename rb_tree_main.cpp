49e0b4ababf8c96fedd34239bd8dd6e3e0e46207
wleite <wleite@student.42sp.org.br>
2022-04-30 03:13:54 -0300

    test: changed red-black tree tests to template

--- rb_tree_main.cpp
+++ rb_tree_main.cpp
@@ -3,16 +3,19 @@
 #include <iostream>
 
 #include "./rb_tree.hpp"
+#include "./utility.hpp"
 
 #define RESET "\033[0m"
 #define BOLDRED "\033[1m\033[31m"   /* Red */
 #define BOLDBLACK "\033[1m\033[30m" /* Bold Black */
 
-void print_helper(const ft::Rb_tree_node::Node_ptr& root,
+#define TYPE ft::pair<int, int>
+
+void print_helper(const ft::_Rb_tree_node<TYPE>::Node_ptr& root,
                   std::string indent,
                   bool last) {
   // print the tree structure on the screen
-  if (root != NULL && root->data != 0) {
+  if (root != NULL && root->data.first != 0) {
     std::cout << indent;
     if (last) {
       std::cout << "R----";
@@ -24,7 +27,7 @@ void print_helper(const ft::Rb_tree_node::Node_ptr& root,
 
     std::string scolor = root->color ? BOLDRED "RED" RESET
                                      : BOLDBLACK "BLACK" RESET;
-    std::cout << root->data << "(" << scolor << ")" << std::endl;
+    std::cout << root->data.first << "(" << scolor << ")" << std::endl;
     print_helper(root->left, indent, false);
     print_helper(root->right, indent, true);
   }
@@ -32,38 +35,36 @@ void print_helper(const ft::Rb_tree_node::Node_ptr& root,
 }
 
 // print the tree structure on the screen
-void pretty_print(ft::Rb_tree_node::Node_ptr root) {
+void pretty_print(ft::_Rb_tree_node<TYPE>::Node_ptr root) {
   if (root) {
     print_helper(root, "", true);
   }
 }
 
-void inorderOrderHelper(ft::Rb_tree_node::Node_ptr node) {
-  if (node != NULL && node->data != 0) {
+void inorderOrderHelper(ft::_Rb_tree_node<TYPE>::Node_ptr node) {
+  if (node != NULL && node->data.first != 0) {
     inorderOrderHelper(node->left);
-    std::cout << node->data << " ";
+    std::cout << node->data.first << " ";
     inorderOrderHelper(node->right);
   }
 }
 
 int main() {
-  ft::Rb_tree bst;
-
-  bst.insert(8);
-  bst.insert(18);
-  bst.insert(5);
-  bst.insert(15);
-  bst.insert(17);
-  bst.insert(25);
-  bst.insert(40);
-  bst.insert(80);
-  bst.insert(1);
-  bst.insert(2);
-  bst.insert(30);
+  ft::Rb_tree<TYPE> bst;
 
   std::cout << "\nBefore delete: \n";
-  pretty_print(bst.get_root());
-  // bst.delete_node(8);
+  bst.insert(TYPE(8, 8));
+  bst.insert(TYPE(18, 18));
+  bst.insert(TYPE(5, 5));
+  bst.insert(TYPE(15, 15));
+  bst.insert(TYPE(17, 17));
+  bst.insert(TYPE(25, 25));
+  bst.insert(TYPE(40, 40));
+  bst.insert(TYPE(80, 80));
+  bst.insert(TYPE(1, 1));
+    pretty_print(bst.get_root());
+
+  //   bst.delete_node(1);
   // bst.delete_node(18);
   // bst.delete_node(5);
   // bst.delete_node(15);
@@ -79,15 +80,16 @@ int main() {
   // bst.delete_node(15);
   //   bst.delete_node(15);
   //   bst.delete_node(8);
-  bst.delete_node(25);
-  std::cout << "\n";
-  std::cout << "After delete: \n";
-  pretty_print(bst.get_root());
-  std::cout << "\n";
-  std::cout << "Search 17: " << bst.search(17)->data << "\n";
-  std::cout << "Minimum: " << bst.minimum(bst.get_root())->data << "\n";
-  std::cout << "Maximum: " << bst.maximum(bst.get_root())->data << "\n";
-  std::cout << "Inorder: ";
-  inorderOrderHelper(bst.get_root());
+  // kkk
+  //   bst.delete_node(25);
+  //   std::cout << "\n";
+  //   std::cout << "After delete: \n";
+//   pretty_print(bst.get_root());
+  //   std::cout << "\n";
+  std::cout << "Search 10: " << bst.search(TYPE(10, 10))->data.first << "\n";
+    std::cout << "Minimum: " << bst.minimum(bst.get_root())->data.first << "\n";
+    std::cout << "Maximum: " << bst.maximum(bst.get_root())->data.first << "\n";
+    std::cout << "Inorder: ";
+    inorderOrderHelper(bst.get_root());
   return 0;
 }
