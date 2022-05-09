8ba51b983f8d201f476557ce9133be799d5809c0
wleite <wleite@student.42sp.org.br>
2022-05-09 16:18:37 -0300

    fix: fixed red-black tree iterator category

--- rb_tree_iterator.hpp
+++ rb_tree_iterator.hpp
@@ -13,7 +13,6 @@ class rb_tree_iterator : public iterator<bidirectional_iterator_tag,
                                          Iterator> {
  public:
   typedef Iterator iterator_type;
-  typedef typename iterator_traits<Iterator>::iterator_category iterator_category;
   typedef typename iterator_traits<Iterator>::value_type value_type;
   typedef typename iterator_traits<Iterator>::difference_type difference_type;
   typedef typename iterator_traits<Iterator>::pointer pointer;
