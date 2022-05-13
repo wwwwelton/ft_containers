d579549d5379722f2256b2d90957d4d8784724f1
wleite <wleite@student.42sp.org.br>
2022-05-13 02:34:14 -0300

    test: added map base class test file

---
+++ test/map/map_test_1.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2022 Welton Leite, wleite. All rights reserved.
+
+#include <iostream>
+
+#define KO "\033[1;31mKO\033[0m"
+#define OK "\033[1;32mOK\033[0m"
+
+#include <map>
+
+#include "../../map.hpp"
+
+int main(void) {
+    std::cout << "\n";
+
+  return (0);
+}
