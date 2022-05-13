f0bdfbbe6e68ca94696344cb72534d005b8160e3
wleite <wleite@student.42sp.org.br>
2022-05-13 03:43:13 -0300

    test: added map void constructor test

--- test/map/map_test_1.cpp
+++ test/map/map_test_1.cpp
@@ -5,12 +5,34 @@
 #define KO "\033[1;31mKO\033[0m"
 #define OK "\033[1;32mOK\033[0m"
 
+#define NEWLINE std::cout << "\n";
+
+#define KEY std::string
+#define VALUE int
+#define TYPE ft::pair<KEY, VALUE>
+
+#define _MAP_T KEY, TYPE
+
 #include <map>
 
 #include "../../map.hpp"
 
 int main(void) {
-    std::cout << "\n";
+  std::cout << "\n===========[ CONSTRUCTORS ]===========\n";
+  {
+    std::cout << "\n[ CONSTRUCTOR VOID ]\n";
+    ft::map<_MAP_T> FTmap;
+
+    ft::map<_MAP_T>::iterator FTit = FTmap.begin();
+    ft::map<_MAP_T>::iterator FTite = FTmap.end();
+
+    if ((FTit->first == "") && (FTite->first == ""))
+      std::cout << OK " ";
+    else
+      std::cout << KO " ";
+  }
+
+  NEWLINE
 
   return (0);
 }
