9838cf9318fc744a93829fd725be139d228dfb9b
wleite <wleite@student.42sp.org.br>
2022-05-13 17:27:16 -0300

    test: added map insert val test

--- test/map/map_test_1.cpp
+++ test/map/map_test_1.cpp
@@ -2,6 +2,14 @@
 
 #include <iostream>
 
+template <typename T>
+void print(const T& x) {
+  std::cout << std::endl;
+  std::cout << "===============" << std::endl;
+  std::cout << x << std::endl;
+  std::cout << "===============" << std::endl;
+}
+
 #define KO "\033[1;31mKO\033[0m"
 #define OK "\033[1;32mOK\033[0m"
 
@@ -9,9 +17,10 @@
 
 #define KEY std::string
 #define VALUE int
-#define TYPE ft::pair<KEY, VALUE>
+#define FT_TYPE ft::pair<KEY, VALUE>
+#define OR_TYPE std::pair<KEY, VALUE>
 
-#define _MAP_T KEY, TYPE
+#define MAP_T KEY, VALUE
 
 #include <map>
 
@@ -21,12 +30,44 @@ int main(void) {
   std::cout << "\n===========[ CONSTRUCTORS ]===========\n";
   {
     std::cout << "\n[ CONSTRUCTOR VOID ]\n";
-    ft::map<_MAP_T> FTmap;
+    ft::map<MAP_T> FTmap;
 
-    ft::map<_MAP_T>::iterator FTit = FTmap.begin();
-    ft::map<_MAP_T>::iterator FTite = FTmap.end();
+    ft::map<MAP_T>::iterator FTit = FTmap.begin();
+    ft::map<MAP_T>::iterator FTite = FTmap.end();
 
     if ((FTit->first == "") && (FTite->first == "") && (FTit == FTite))
+      std::cout << OK " ";
+    else
+      std::cout << KO " ";
+  }
+
+  NEWLINE
+
+  std::cout << "\n\n===========[ MAP MODIFIERS ]===========\n";
+  {
+    std::cout << "\n[ INSERT VAL ]\n";
+    ft::map<MAP_T> FTmap;
+    std::map<MAP_T> ORmap;
+
+    ft::pair<ft::map<MAP_T>::iterator, bool> FTit;
+    std::pair<std::map<MAP_T>::iterator, bool> ORit;
+
+    FTit = FTmap.insert(FT_TYPE("a", 1));
+    ORit = ORmap.insert(OR_TYPE("a", 1));
+
+    if ((FTit.first->first == ORit.first->first) &&
+        (FTit.first->second == ORit.first->second) &&
+        (FTit.second == ORit.second))
+      std::cout << OK " ";
+    else
+      std::cout << KO " ";
+
+    FTit = FTmap.insert(FT_TYPE("a", 1));
+    ORit = ORmap.insert(OR_TYPE("a", 1));
+
+    if ((FTit.first->first == ORit.first->first) &&
+        (FTit.first->second == ORit.first->second) &&
+        (FTit.second == ORit.second))
       std::cout << OK " ";
     else
       std::cout << KO " ";
