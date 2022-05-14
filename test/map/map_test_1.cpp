3218a6cc335c685fbba579459f568d924376779d
wleite <wleite@student.42sp.org.br>
2022-05-14 01:40:32 -0300

    test: added map empty test

--- test/map/map_test_1.cpp
+++ test/map/map_test_1.cpp
@@ -241,6 +241,28 @@ int main(void) {
       std::cout << KO " ";
   }
 
+  std::cout << "\n\n===========[ CAPACITY ]===========\n";
+  {
+    std::cout << "\n[ EMPTY ]\n";
+    ft::map<MAP_T> FTmap;
+    std::map<MAP_T> ORmap;
+
+    if (FTmap.empty() == ORmap.empty())
+      std::cout << OK " ";
+    else
+      std::cout << KO " ";
+
+    FTmap.insert(FT_TYPE("a", 8)), ORmap.insert(OR_TYPE("a", 8));
+    FTmap.insert(FT_TYPE("b", 18)), ORmap.insert(OR_TYPE("b", 18));
+    FTmap.insert(FT_TYPE("c", 5)), ORmap.insert(OR_TYPE("c", 5));
+    FTmap.insert(FT_TYPE("d", 15)), ORmap.insert(OR_TYPE("d", 15));
+
+    if (FTmap.empty() == ORmap.empty())
+      std::cout << OK " ";
+    else
+      std::cout << KO " ";
+  }
+
   std::cout << "\n\n===========[ MAP MODIFIERS ]===========\n";
   {
     std::cout << "\n[ INSERT VAL ]\n";
