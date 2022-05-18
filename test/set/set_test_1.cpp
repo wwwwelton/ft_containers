44a520ef29803ab4322801663d93bed3869c61e7
wleite <wleite@student.42sp.org.br>
2022-05-17 22:34:50 -0300

    test: added set test file

---
+++ test/set/set_test_1.cpp
@@ -0,0 +1,42 @@
+// Copyright (c) 2022 Welton Leite, wleite. All rights reserved.
+
+#include <iostream>
+
+template <typename T>
+void print(const T& x) {
+  std::cout << std::endl;
+  std::cout << "===============" << std::endl;
+  std::cout << x << std::endl;
+  std::cout << "===============" << std::endl;
+}
+
+template <typename T>
+void print_set(T& set) {
+  for (typename T::iterator it = set.begin(); it != set.end(); it++) {
+    std::cout << "[" << *it << "]"
+              << " ";
+  }
+  std::cout << std::endl;
+}
+
+#define KO "\033[1;31mKO\033[0m"
+#define OK "\033[1;32mOK\033[0m"
+
+#define NEWLINE std::cout << "\n";
+
+#define KEY_VALUE std::string
+#define SET_T KEY_VALUE
+
+#define FT_PRINT ft::set<SET_T>
+#define OR_PRINT std::set<SET_T>
+
+#include <set>
+
+#include "../../algorithm.hpp"
+#include "../../set.hpp"
+
+int main(void) {
+  NEWLINE
+
+  return (0);
+}
