184a5c99f2c3cb5a46b3cc25542ab8af52fb59c0
wleite <wleite@student.42sp.org.br>
2022-05-17 23:40:49 -0300

    test: added set different relational operator test

--- test/set/set_test_1.cpp
+++ test/set/set_test_1.cpp
@@ -672,6 +672,28 @@ int main(void) {
     else
       std::cout << KO " ";
   }
+  {
+    std::cout << "\n[ DIFFERENT (!=) ]\n";
+    ft::set<SET_T> FTset_copy;
+    ft::set<SET_T> FTset;
+
+    FTset_copy.insert("a"), FTset.insert("a");
+    FTset_copy.insert("b"), FTset.insert("b");
+    FTset_copy.insert("c"), FTset.insert("c");
+    FTset_copy.insert("d"), FTset.insert("e");
+
+    if (FTset != FTset_copy)
+      std::cout << OK " ";
+    else
+      std::cout << KO " ";
+
+    FTset_copy.insert("e"), FTset.insert("d");
+
+    if (!(FTset != FTset_copy))
+      std::cout << OK " ";
+    else
+      std::cout << KO " ";
+  }
 
   NEWLINE
 
