f34ddc3cb46fc8ff3c1583ec0a6a3d0d4a900940
wleite <wleite@student.42sp.org.br>
2022-05-20 17:12:19 -0300

    test: added vector constructor speed test

--- test/vector/vector_test_3.cpp
+++ test/vector/vector_test_3.cpp
@@ -5,6 +5,7 @@
 #define KO "\033[1;31mKO\033[0m"
 #define OK "\033[1;32mOK\033[0m"
 
+#include <climits>
 #include <ctime>
 #include <iomanip>
 #include <vector>
@@ -12,50 +13,93 @@
 #include "../../algorithm.hpp"
 #include "../../vector.hpp"
 
-#define K10 100000
+#define AMOUNT 100000
 
-void print_time(int option) {
+#define FT 42
+#define STD 24
+#define MAX 22
+
+void print_time(int option = -1, int NAMESPACE = 0) {
   static clock_t t = 0;
   static double time_taken = 0;
+  static double total_time_ft = 0;
+  static double total_time_std = 0;
 
   if (option == 1) {
+    if (NAMESPACE == FT) {
+      std::cout << "FT : ";
+    }
+    if (NAMESPACE == STD) {
+      std::cout << "STD: ";
+    }
     t = clock();
   }
   if (option == 2) {
     t = clock() - t;
     time_taken = static_cast<double>(t) / CLOCKS_PER_SEC;
     std::cout << std::setprecision(3) << time_taken << std::endl;
+    if (NAMESPACE == FT) {
+      total_time_ft += time_taken;
+    }
+    if (NAMESPACE == STD) {
+      total_time_std += time_taken;
+    }
   }
-  if (option == 3) {
+  if (option == MAX) {
+    std::cout << "MAX: ";
     std::cout << std::setprecision(3) << (time_taken * 20) << std::endl;
+  }
+  if (option == 4) {
+    std::cout << "\n[ TOTAL TIME ]\n";
+
+    std::cout << "FT : ";
+    std::cout << std::setprecision(3) << total_time_ft << std::endl;
+
+    std::cout << "STD: ";
+    std::cout << std::setprecision(3) << total_time_std << std::endl;
+
+    std::cout << "MAX: ";
+    std::cout << std::setprecision(3) << (total_time_std * 20) << std::endl;
   }
 }
 
 int main(void) {
   std::cout << "\n===========[ VECTOR ]===========\n";
   {
+    std::cout << "\n[ CONSTRUCTOR ]\n";
+
+    print_time(1, FT);
+    ft::vector<int> FTvec(AMOUNT, INT_MAX);
+    print_time(2, FT);
+
+    print_time(1, STD);
+    ft::vector<int> ORvec(AMOUNT, INT_MAX);
+    print_time(2, STD);
+
+    print_time(MAX);
+  }
+
+  {
     std::cout << "\n[ INSERT ]\n";
     ft::vector<int> FTvec;
     std::vector<int> ORvec;
 
-    std::cout << "FT : ";
-    print_time(1);
-    for (int i = 0; i < K10; i++) {
+    print_time(1, FT);
+    for (int i = 0; i < AMOUNT; i++) {
       FTvec.insert(FTvec.begin(), i);
     }
-    print_time(2);
+    print_time(2, FT);
 
-    std::cout << "STD: ";
-    print_time(1);
-    for (int i = 0; i < K10; i++) {
+    print_time(1, STD);
+    for (int i = 0; i < AMOUNT; i++) {
       ORvec.insert(ORvec.begin(), i);
     }
-    print_time(2);
+    print_time(2, STD);
 
-    std::cout << "MAX: ";
-    print_time(3);
+    print_time(MAX);
   }
 
+  print_time(4);
   std::cout << "\n";
 
   return (0);
