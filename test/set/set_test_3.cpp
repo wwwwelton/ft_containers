680f012f61ba069e2e9238ab7ec1b29deed5df61
wleite <wleite@student.42sp.org.br>
2022-05-21 18:26:23 -0300

    test: added set find speed test

--- test/set/set_test_3.cpp
+++ test/set/set_test_3.cpp
@@ -161,6 +161,35 @@ int main(void) {
     print_time(MAX);
   }
 
+  {
+    std::cout << "\n[ FIND ]\n";
+
+    ft::set<std::string> FTset;
+    std::set<std::string> ORset;
+
+    for (int i = 0; i < AMOUNT; i++) {
+      ss << i << " ", ss >> str;
+      FTset.insert(str);
+      ORset.insert(str);
+    }
+
+    print_time(1, FT);
+    for (int i = 0; i < AMOUNT; i++) {
+      ss << AMOUNT << " ", ss >> str;
+      FTset.find(str);
+    }
+    print_time(2, FT);
+
+    print_time(1, STD);
+    for (int i = 0; i < AMOUNT; i++) {
+      ss << AMOUNT << " ", ss >> str;
+      ORset.find(str);
+    }
+    print_time(2, STD);
+
+    print_time(MAX);
+  }
+
   print_time(4);
   std::cout << "\n";
 
