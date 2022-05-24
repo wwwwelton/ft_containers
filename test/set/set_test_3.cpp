b54dc4dd1fcccaefa148b3d246e06cc087df77a8
wleite <wleite@student.42sp.org.br>
2022-05-24 00:02:21 -0300

    test: added set size speed test

--- test/set/set_test_3.cpp
+++ test/set/set_test_3.cpp
@@ -178,6 +178,33 @@ int main(void) {
   }
 
   {
+    std::cout << "\n[ SIZE ]\n";
+
+    ft::set<std::string> FTset;
+    std::set<std::string> ORset;
+
+    for (int i = 0; i < AMOUNT; i++) {
+      ss << i << " ", ss >> str;
+      FTset.insert(str);
+      ORset.insert(str);
+    }
+
+    print_time(1, FT);
+    for (int i = 0; i < AMOUNT; i++) {
+      FTset.size();
+    }
+    print_time(2, FT);
+
+    print_time(1, STD);
+    for (int i = 0; i < AMOUNT; i++) {
+      ORset.size();
+    }
+    print_time(2, STD);
+
+    print_time(MAX);
+  }
+
+  {
     std::cout << "\n[ INSERT ]\n";
 
     ft::set<std::string> FTset;
