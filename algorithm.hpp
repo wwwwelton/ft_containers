a6fb2dfda51f0bb980fe1472274f6e886ac42a69
wleite <wleite@student.42sp.org.br>
2022-05-25 23:18:07 -0300

    refactor: changed lexicographical_compare

--- algorithm.hpp
+++ algorithm.hpp
@@ -33,7 +33,7 @@ inline bool equal(InputIt1 first1, InputIt1 last1, InputIt2 first2,
 template <typename InputIt1, typename InputIt2>
 inline bool lexicographical_compare(InputIt1 first1, InputIt1 last1,
                                     InputIt2 first2, InputIt2 last2) {
-  for (; (first1 != last1) && (first2 != last2); ++first1, ++first2) {
+  for (; (first1 != last1) && (first2 != last2); ++first1, (void)++first2) {
     if (*first1 < *first2) {
       return (true);
     }
